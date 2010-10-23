0132a5a653a48e84c2b10a10a60c11219771ba1e
David Siegel <david.siegel@artcom.de>
2010-10-23 13:13:26 +0200

    added some more authentication stuff; some progress on message creation

---
+++ tests/test-0020-message.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+
+var dbus   = require('../lib/low_level')
+  , assert = require('assert')
+  ;
+
+assert.throws(function() { dbus.createMethodCall() });
+assert.throws(function() { dbus.createMethodCall('narf')});
+assert.throws(function() { dbus.createMethodCall('narf', 'barf')});
+assert.throws(function() { dbus.createMethodCall('narf', 'barf', 'snarf')});
+
+var msg = dbus.createMethodCall('org.example.NodeDBusTest', '/', 'Method');
+
